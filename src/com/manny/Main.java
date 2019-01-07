b802118a5898cc31d5e58068b33ead4665b162e2
Manny Aguilar <mannyaaguilar@yahoo.com>
2019-01-07 01:08:46 -0700

    first commit

---
+++ src/com/manny/Main.java
@@ -0,0 +1,15 @@
+package com.manny;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i + " is an odd number");
+            }
+
+        }
+    }
+}
